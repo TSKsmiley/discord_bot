cc7cd1001aefdd5c62021f2b80cba03b363d0cea
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 15:50:36 +0200

    main.py self updates
    updater.py has been moved into main.py

--- main.py
+++ main.py
@@ -128,4 +128,6 @@ async def on_error(error):
 
 #running the bot
 client.run(BOT_TOKEN)
-os.system("python3 updater.py")
+os.system("rm main.py")
+os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/e78e6bfa6b59fb099142d7c1dd791de8b1e0c64b/main.py")
+os.system("python3 main.py")
