eb0278879cd2baf4a9844a9f5dfce5ee90a895ac
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 15:18:17 +0200

    final updater 1.0
    i hope

--- updater.py
+++ updater.py
@@ -1,10 +1,6 @@
 import os
-import asyncio
-import subprocess
 
 os.system("rm main.py")
 os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/e78e6bfa6b59fb099142d7c1dd791de8b1e0c64b/main.py")
 os.system("python3 main.py")
 
-#subprocess.check_output(['ls','-l']) #all that is technically needed...
-print subprocess.check_output(['python3', 'main.py'])
