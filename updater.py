91eb3d980cacdd1ec0d4d0a96495033a1d29afaf
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 15:34:44 +0200

    commit

--- updater.py
+++ updater.py
@@ -1,6 +1,8 @@
 import os
+import time
 
 os.system("rm main.py")
 os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/e78e6bfa6b59fb099142d7c1dd791de8b1e0c64b/main.py")
+time.sleep(10)
 os.system("python3 main.py")
 
