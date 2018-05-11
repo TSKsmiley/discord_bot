950bd3212f807e407da764d953d8ce08e85a1335
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 18:40:22 +0200

    removed unused file

--- updater.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-
-os.system("rm main.py")
-os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/e78e6bfa6b59fb099142d7c1dd791de8b1e0c64b/main.py")
-os.system("python3 main.py")
-
