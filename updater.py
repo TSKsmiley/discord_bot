b0ae8fce461d3b0196775e2e0bd927d2515d5ee3
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 14:42:27 +0200

    s
    s

--- updater.py
+++ updater.py
@@ -9,6 +9,6 @@ def test():
     os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/master/main.py")
     await asyncio.sleep(9)
     os.system("python3 main.py")
-    exit()
 #done
 async test()
+exit()
