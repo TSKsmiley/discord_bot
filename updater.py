6cf2c0874b5a6084e5f156b6a9adfde12434e347
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 13:45:51 +0200

    msg

--- updater.py
+++ updater.py
@@ -1,10 +1,6 @@
 import os
 
-async def update_and_run():
-    await os.system("rm main.py")
-    await os.system(
-        "wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/master/main.py")
-    await os.system("python3 main.py")
-    exit()
-
-update_and_run()
+os.system("rm main.py")
+os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/master/main.py")
+ os.system("python3 main.py")
+exit()
