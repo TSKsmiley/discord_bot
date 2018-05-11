d42dc35eb2f0ab6a51327d1a06455ac5aeb59eac
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 13:40:01 +0200

    fixed updater

--- updater.py
+++ updater.py
@@ -1,6 +1,7 @@
 import os
 
 async def update_and_run():
+    os.system("rm main.py")
     os.system(
         "wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/master/main.py")
-    os.system(python3 main.py)
+    os.system("python3 main.py")
