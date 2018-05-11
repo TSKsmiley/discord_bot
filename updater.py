a91a1f8d9f210573f64ea2bae2c27faf0618e6f8
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-11 13:46:33 +0200

    s
    s

--- updater.py
+++ updater.py
@@ -2,5 +2,5 @@ import os
 
 os.system("rm main.py")
 os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/master/main.py")
- os.system("python3 main.py")
+os.system("python3 main.py")
 exit()
