0beac92b907be7dcff8fe5bf9490d6640c6b1164
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-13 21:34:37 +0200

    fixed "auto" update

--- main.py
+++ main.py
@@ -138,5 +138,5 @@ client.run(BOT_TOKEN)
 #auto updating and restarting (linux)
 if(os.name != "nt"):
     os.system("rm main.py")
-    os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_weather_bot/e78e6bfa6b59fb099142d7c1dd791de8b1e0c64b/main.py")
+    os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_bot/e78e6bfa6b59fb099142d7c1dd791de8b1e0c64b/main.py")
     os.system("python3 main.py")
