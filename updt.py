a3f0640f29d401f6750ca0ccc5b6e487d1b6644d
TSKsmiley <37146398+TSKsmiley@users.noreply.github.com>
2018-05-14 10:41:33 +0200

    importing..

--- updt.py
+++ updt.py
@@ -1,5 +1,8 @@
-    os.system("rm main.py")
-    asyncio.sleep(3)
-    os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_bot/master/main.py")
-    asyncio.sleep(20)
-    os.system("python3 main.py")
+import os
+import asyncio
+
+os.system("rm main.py")
+asyncio.sleep(3)
+os.system("wget https://raw.githubusercontent.com/TSKsmiley/discord_bot/master/main.py")
+asyncio.sleep(20)
+os.system("python3 main.py")
